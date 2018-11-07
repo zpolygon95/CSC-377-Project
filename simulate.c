cf8a5797a5df1b6f460b931a9f1c228719dd1314
Zachary Polikarpus (empoleon) <zpolygon95@gmail.com>
2018-11-07 10:35:27 -0500

    Change dereference to address-of operators

--- simulate.c
+++ simulate.c
@@ -62,7 +62,7 @@ int main(int argc, char * const argv[])
         fprintf(stderr, "manager: starting...\n");
         while(status)
         {
-            error = read(pipefd[1], *in, 1);
+            error = read(pipefd[1], &in, 1);
             switch (error) {
                 case -1:
                     perror("manager: read() failed");
@@ -82,7 +82,7 @@ int main(int argc, char * const argv[])
         int in;
         while ((in = getchar()) != EOF)
         {
-            write(pipefd[0], *in, 1);
+            write(pipefd[0], &in, 1);
             sleep(SLEEPY_TIME);
         }
     }
