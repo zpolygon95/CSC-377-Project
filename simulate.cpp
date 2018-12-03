fe704a524d7004d302d6c0d09c5ea3dd356f1f54
Zachary Polikarpus (empoleon) <zpolygon95@gmail.com>
2018-12-02 21:57:32 -0500

    Implement better instruction parsing

--- simulate.cpp
+++ simulate.cpp
@@ -3,10 +3,27 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
-map<string, vector<string>> files;
+class instruction_t {
+public:
+    string opcode;
+    int arg_int;
+    string arg_string;
+
+    instruction_t (string line)
+    {
+        opcode = line[0];
+        arg_int =
+            (opcode == "R" || line.size() <= 2) ? 0 : stoi(line.substr(2));
+        arg_string =
+            (opcode != "R" || line.size() <= 2) ? "" : line.substr(2);
+    }
+};
+
+map<string, vector<instruction_t>> files;
 
 void parse_files(string file)
 {
@@ -14,19 +31,19 @@ void parse_files(string file)
     {
         ifstream in(file);
         string str;
-        vector<string> instructions;
+        vector<instruction_t> instructions;
         while(getline(in, str))
         {
             if (str.size() > 0)
-                instructions.push_back(str);
+                instructions.push_back(instruction_t(str));
         }
         if (instructions.size() > 0)
-            files.insert(pair<string, vector<string>>(file, instructions));
+            files.insert(pair<string, vector<instruction_t>>(file, instructions));
         for (int i = 0; i < instructions.size(); i++)
         {
-            if (instructions[i][0] == 'R')
+            if (instructions[i].opcode == "R")
             {
-                string nextpath = instructions[i].substr(2);
+                string nextpath = instructions[i].arg_string;
                 parse_files(nextpath);
             }
         }
@@ -42,10 +59,14 @@ int main(int argc, char const *argv[]) {
     bool fast = (argc > 2) && (string(argv[2]) == "-f");
     string path(argv[1]);
     parse_files(path);
-    map<string, vector<string>>::iterator it;
+    map<string, vector<instruction_t>>::iterator it;
     for(it = files.begin(); it != files.end(); it++)
     {
-        cout << it->first << " => " << it->second.size() << endl;
+        cout << it->first << " => " << endl;
+        for (int i = 0; i < it->second.size(); i++)
+        {
+            cout << "  " << it->second[i].opcode << " " << it->second[i].arg_int << "|" << it->second[i].arg_string << endl;
+        }
     }
     return 0;
 }
