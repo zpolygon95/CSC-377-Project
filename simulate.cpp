0a54b33eb3285346f7730307004ffa1a2d70601a
Zachary Polikarpus (empoleon) <zpolygon95@gmail.com>
2018-12-02 22:44:02 -0500

    Merge work from C file

--- simulate.cpp
+++ simulate.cpp
@@ -4,6 +4,10 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -23,7 +27,21 @@ public:
     }
 };
 
+typedef struct
+{
+    int pid;
+    int ppid;
+    int pc;
+    int value;
+    int priority;
+    int state;
+    int startt;
+    int runt;
+} process_t;
+
 map<string, vector<instruction_t>> files;
+int pipefd[2];
+int SLEEPY_TIME = 1;
 
 void parse_files(string file)
 {
@@ -50,6 +68,26 @@ void parse_files(string file)
     }
 }
 
+void tick()
+{
+    // TODO
+}
+
+void unblock()
+{
+    // TODO
+}
+
+int mgrHandleInput(char input)
+{
+    // TODO: handle input
+    return 0;
+}
+
+void printReport()
+{
+    // TODO: print report
+}
 
 int main(int argc, char const *argv[]) {
     if (argc < 2) {
@@ -68,5 +106,51 @@ int main(int argc, char const *argv[]) {
             cout << "  " << it->second[i].opcode << " " << it->second[i].arg_int << "|" << it->second[i].arg_string << endl;
         }
     }
+
+    // commander process
+    pipe(pipefd);
+    // spawn process manager
+    pid_t pmanager = fork();
+    if (pmanager < 0)
+    {
+        perror("commander: fork() failed");
+        exit(EXIT_FAILURE);
+    }
+    else if (pmanager == 0)
+    {
+        // manager process
+        char in;
+        int error;
+        int status = 1;
+        fprintf(stderr, "manager: starting...\n");
+        while(status)
+        {
+            error = read(pipefd[0], &in, 1);
+            switch (error) {
+                case -1:
+                    perror("manager: read() failed");
+                case 0:
+                    status = 0;
+                    break;
+                default:
+                    status = mgrHandleInput(in);
+                    break;
+            }
+        }
+        fprintf(stderr, "manager: done\n");
+    }
+    else
+    {
+        // commander process
+        int in;
+        fprintf(stderr, "commander: starting...\n");
+        while ((in = getchar()) != EOF)
+        {
+            write(pipefd[1], &in, 1);
+            sleep(SLEEPY_TIME);
+        }
+        fprintf(stderr, "commander: done\n");
+    }
+
     return 0;
 }
